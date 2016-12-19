e38f9d6123ae7dfa2a313ce826ad08f01085a1e5
dackow <dackow@gmail.com>
2016-12-19 21:20:12 +0100

    Model 1

---
+++ CzteryPokoje/Models/OfferModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CzteryPokoje.Models
+{
+    public class Offer
+    {
+        [Required]
+        public int  ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Price { get; set; }
+        [Required]
+        public string Status { get; set; }
+        
+
+    }
+}
